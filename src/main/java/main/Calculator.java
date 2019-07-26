aa34fbbe566d8b8e671eea7d444d1560eed23224
Devyansh <Devyansh@DESKTOP-6N379BI>
2019-07-26 23:19:08 +0530

    all functions runs successfully

--- src/main/java/main/Calculator.java
+++ src/main/java/main/Calculator.java
@@ -13,51 +13,40 @@ public class Calculator {
         System.out.println("Enter your selection: 1 for Addition, 2 for substraction 3 for Multiplication and 4 for division:");
         int choose;
         choose = inp.nextInt();
-        switch (choose){
-        case 1:
+        if(choose==1)
             System.out.println(add( num1,num2));
-            break;
-        case 2:
-            System.out.println(sub( num1,num2));
-            break;      
-        case 3:
+        else if(choose==2)
+            System.out.println(sub( num1,num2));  
+        else if(choose==3)
             System.out.println(mult( num1,num2));
-            break;
-        case 4:
+        else if(choose==4)
             System.out.println(div( num1,num2));
-            break;
-            default:
+        else
                 System.out.println("Illigal Operation");
-
-
-        }
-
-
-
     }
     public static int add(int x, int y)
     {
-    	int result=0;
 //        write your code here 
+    	int result=x+y;
     	
         return result;
     }
     public static int sub(int x, int y)
     {
-    	int result=0;
 //        write your code here
+    	int result=x-y;
         return result;
     }
     public static int mult(int x, int y)
     {
-    	int result=0;
-//      write your code here        
+//      write your code here
+    	int result=x*y;
     	return result;
     }
     public static int div(int x, int y)
     {
-    	int result=0;
 //      write your code here
+    	int result=x/y;
     	return result;
     }
 
